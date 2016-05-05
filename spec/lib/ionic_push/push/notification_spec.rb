080ebc4f9be700cda7314e1c30205c95d3f0d3cc
Thiago F. Lins <thiago.evandson@gmail.com>
2016-05-04 22:22:55 -0300

    refacotr notificatin spec

--- spec/lib/ionic_push/push/notification_spec.rb
+++ spec/lib/ionic_push/push/notification_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe IonicPush::Push::Notification do
   describe '#body' do
-    before do
-      @notification = IonicPush::Push::Notification.new(['my', 'tokens'], {title: 'My Title', message: 'My Message'})
+    let!(:notification) do
+      IonicPush::Push::Notification.new(['my', 'tokens'], {title: 'My Title', message: 'My Message'})
     end
 
     it "returns a hash" do
-      expect(@notification.body).to be_a_kind_of(Hash)
+      expect(notification.body).to be_a_kind_of(Hash)
     end
 
     it "includes a notification body" do
       notification_body = { title: 'My Title', message: 'My Message'}
-      expect(@notification.body).to include(notification: notification_body)
+      expect(notification.body).to include(notification: notification_body)
     end
 
     it "includes a device tokens" do
       tokens = { tokens: ['my', 'tokens'] }
-      expect(@notification.body).to include(tokens)
+      expect(notification.body).to include(tokens)
     end
   end
 end
