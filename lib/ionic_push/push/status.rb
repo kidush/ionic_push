379b2b2015651141352af6862f863b096915506a
Thiago F. Lins <thiago.evandson@gmail.com>
2016-05-17 23:36:40 -0300

    Status implemented but needs to be a lot of improvements :)

--- lib/ionic_push/push/status.rb
+++ lib/ionic_push/push/status.rb
@@ -1,13 +1,17 @@
 module IonicPush
   module Push
     class Status
+      attr_reader :created, :state, :uuid, :config, :app_id, :status
       def initialize(request)
-        @request = request
+        @request = request["data"]
+        @created = @request["created"]
+        @state = @request["state"]
+        @uuid = @request["uuid"]
+        @config = @request["config"]
+        @app_id = @request["app_id"]
+        @status = @request["status"]
       end
 
-      def state
-        @request["data"]["state"]
-      end
     end
   end
 end
